90bd3e3cad078dce8dc8ba4e9d73923f6cd47e8d
Nabil-Abdullahi <91533621+Nabil-Abdullahi@users.noreply.github.com>
2024-04-23 11:26:40 +0100

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,3 +1,4 @@
-print(16*12)
-print("Checking to see if pull request actions works")
-print("Multiple checks")
+def printing():
+  print(16*12)
+  print("Checking to see if pull request actions works")
+  print("Multiple checks")
