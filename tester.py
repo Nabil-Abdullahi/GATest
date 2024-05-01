515abe98d0177a55973941144414a7b40d1e07dd
Nabil-Abdullahi <91533621+Nabil-Abdullahi@users.noreply.github.com>
2024-05-01 16:08:36 +0100

    Update tester.py

--- tester.py
+++ tester.py
@@ -3,4 +3,4 @@ def printing():
   print(16*12)
   print("Checking to see if pull request actions works")
   print("Multiple checks")
-access_token="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"
+access_token="comcast010524"
