d27cd4018eb9d3a1f3f6d819c02291465ea82d60
Nabil-Abdullahi <91533621+Nabil-Abdullahi@users.noreply.github.com>
2024-05-02 10:12:41 +0100

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,6 +1,7 @@
-# comcast010524.test
+# github.com/Nabil-Abdullahi/GATest
 def printing():
   print(16*12)
   print("Checking to see if pull request actions works")
   print("Multiple checks")
-a_t="comcast010524"
+a_t="GATest23456789"
+access_token=1234567890998765456789098765467865425647586756474
