e34f55132811e782099b1de1c7ea58fcf831f0e9
Nabil-Abdullahi <91533621+Nabil-Abdullahi@users.noreply.github.com>
2024-05-08 14:22:40 +0100

    Update pol.py

--- pol.py
+++ pol.py
@@ -1,6 +1,6 @@
 # github.com/Nabil-Abdullahi/GATest
-a_t="GATest23456789"
+api_key="127386328297317-39139074309-391070397409"
 access_token="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"
 print("hello")
 for i inrange(0, 5);
-  print("hi
+  print("hi)
