421861aeebb013243fc38a33177dbc80e5a1c883
Nabil-Abdullahi <91533621+Nabil-Abdullahi@users.noreply.github.com>
2024-05-10 16:09:36 +0100

    Update pol.py

--- pol.py
+++ pol.py
@@ -2,6 +2,7 @@
 api_key="127386328297317-39139074309-391070397409"
 access_token="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"
 print("hello")
-for i inrange(0, 5);
-  print("hi)
--
+d = input((str("Hello: ")))
+if type(d) == str:
+  for i in range(0, 5);
+    print(access_token)
