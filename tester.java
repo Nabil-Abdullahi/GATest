5c7121bb315d3a1a3dc4fad69cc3c9e3a3e3199e
Nabil-Abdullahi <91533621+Nabil-Abdullahi@users.noreply.github.com>
2024-05-14 14:50:45 +0100

    Update tester.java

--- tester.java
+++ tester.java
@@ -2,6 +2,6 @@ public class Main {
   public static void main(String[] args) {
     num = "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"
     access_token = "12345redfg5ghiuelasgn4c7484wt3"
-    System.out.println("H");
+    System.out.println("Hi");
   }
 }
