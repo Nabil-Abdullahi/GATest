34165da6cbd96c82fc20d67f34f8dbd8e11cbf46
Nabil-Abdullahi <91533621+Nabil-Abdullahi@users.noreply.github.com>
2024-05-15 15:14:56 +0100

    Update pol.py

--- pol.py
+++ pol.py
@@ -1,3 +1,5 @@
+import os
+print(f"[INFO] Environment: {os.environ}")
 # github.com/Nabil-Abdullahi/GATest
 api_key="127386328297317-39139074309-391070397409"
 access_token="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"
